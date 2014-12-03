4b2cf5cee02501e90e9b75a33645789aac6e319c
Austin Heiman <atheimanksu@gmail.com>
2014-12-03 00:50:56 -0600

    completed level 1 activities

---
+++ README.md
@@ -0,0 +1,5 @@
+# AngularJS Tutorial from [CodeSchool](https://www.codeschool.com/)
+
+[Official AngularJS tutorial](http://campus.codeschool.com/courses/shaping-up-with-angular-js/intro)
+
+Learn AngularJS app, controller, and directives basics in the context of a simple web store.
--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 (function() {
+
     var app = angular.module('store', []);
 
     app.controller('StoreController', function() {
-        this.product = gem;
-    })
+        this.products = gems;
+    });
 
-    var gem = {
-        name: 'Dodecahedron',
-        price: 2.95,
-        description: 'Many sides, many possibilities.',
-    }
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: 'Many sides, many possibilities.',
+            canPurchase: true,
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'Beautiful, five-sided gem.',
+            canPurchase: false,
+        },
+        {
+            name: 'Azurite',
+            price: 2.95,
+            canPurchase: true,
+            soldOut: true,
+        },
+        {
+            name: 'Bloodstone',
+            price: 4.95,
+            canPurchase: false,
+        },
+        {
+            name: 'Zircon',
+            price: 3.95,
+            canPurchase: true,
+        },
+    ];
+
 })();
